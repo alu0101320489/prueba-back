182d1be9100c8a3d55db5d70d74458dbb6646868
Daniel Hernández Fajardo <alu0101320489@ull.edu.es>
2023-01-22 19:09:48 +0000

    Delete backend/dist directory

--- backend/dist/models/usuario.d.ts
+++
@@ -1,8 +0,0 @@
-import { Document } from 'mongoose';
-interface Usuario extends Document {
-    nombre: string;
-    contraseña: string;
-    equipo: string[];
-}
-export declare const Usuario: import("mongoose").Model<Usuario, {}, {}>;
-export {};
